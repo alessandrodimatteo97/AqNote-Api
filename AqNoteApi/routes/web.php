479afb515c079958148bacb455258002139aaa26
davidefontana96 <davidefontana96.df@gmail.com>
2019-07-01 21:16:59 +0200

    correction route

--- AqNoteApi/routes/web.php
+++ AqNoteApi/routes/web.php
@@ -36,36 +36,18 @@ Route::get('/api/Departments', 'Controller@DepartmentsList');
 
 Route::get('/user', 'Controller@prova1');
 $router->group(['prefix' => 'api/'], function ($router) {
-$router->get('login/','UsersController@authenticate');
-$router->get('signup/', 'UsersController@signUp');
-$router->post('todo/','ToDoController@store');
-$router->get('todo/', 'ToDoController@index');
-$router->get('todo/{id}/', 'ToDoController@show');
-$router->put('todo/{id}/', 'ToDoController@update');
-$router->delete('todo/{id}/', 'ToDoController@destroy');
-//$router->get('{id}/', 'UsersController@infoUser');
-
-  $router->group(['prefix' => 'department/'], function($router){
-    $router->get('/', 'DepartmentsController@listDepart');
-
-    $router->group(['prefix' => '{idD}/cdl/'], function($router){
-      $router->get('/', 'CdlsController@listCdl');
-
-      $router->group(['prefix' => '{idC}/year'], function($router){
-        $router->get('/', 'SubjectsController@listSubject');
-
-        $router->group(['prefix' => '{year}/subject'], function($router){
-          $router->get('/', 'SubjectsController@listSubYear');
-
-          $router->group(['prefix' => '{idS}/notes'], function($router){
-            $router->get('/', 'SubjectsController@notesList');
-
-            $router->group(['prefix' => '{idN}'], function($router){
-              $router->get('/', 'SubjectsController@notesDetail');
-            });
-          });
-        });
-      });
-    });
-  });
+  $router->get('login/','UsersController@authenticate');
+  $router->get('signup/', 'UsersController@signUp');
+  $router->post('todo/','ToDoController@store');
+  $router->get('todo/', 'ToDoController@index');
+  $router->get('todo/{id}/', 'ToDoController@show');
+  $router->put('todo/{id}/', 'ToDoController@update');
+  $router->delete('todo/{id}/', 'ToDoController@destroy');
+  //$router->get('{id}/', 'UsersController@infoUser');
+  $router->get('department/', 'DepartmentsController@listDepart');
+  $router->get('cdl/{idD}', 'CdlsController@listCdl');
+  $router->get('subjectlist/{idC}', 'SubjectsController@listSubject');
+  $router->get('subjectlist/{idC}/{year}', 'SubjectsController@listSubYear');
+  $router->get('notesList/{idS}', 'SubjectsController@notesList');
+  $router->get('notes/{idN}', 'SubjectsController@notesDetail');
 });
