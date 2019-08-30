468fd1bc91095f72ab09f83072eeb9d52ef87bfd
alessandrodimatteo97 <alessandrodimatteo1997>
2019-08-31 01:00:00 +0200

    query more or less working

--- AqNoteApi/app/Http/Controllers/ExampleController.php
+++ AqNoteApi/app/Http/Controllers/ExampleController.php
@@ -18,8 +18,8 @@ class ExampleController extends Controller
         //return $idDc;
 
         $hello = DB::select(DB::raw(" SELECT *
-                                                FROM (SELECT u.name, n.idN ,s.nameS, s.year, n.title, n.description ,count(c.note_id) as comments,avg(c.like)-1 as avarage  from subjects as s  join notes as n on n.subject_id=s.id inner join comments as c on n.idN=c.note_id join users u on u.idU = n.user_id join photos p on n.idN = p.note_id where s.degreeCourse_id ='$idDc[0]' group by s.nameS, c.note_id
-                                                )  t1  join (SELECT  note_id as idN,count(note_id) FROM  AqNoteApi.photos join  notes n on n.idN = photos.idP group by note_id)  t2
+                                                FROM (SELECT u.name, n.idN ,s.nameS, s.year, n.title, n.description ,count(c.note_id) as comments,avg(c.like) as avarage  from subjects as s  join notes as n on n.subject_id=s.id join comments as c on n.idN=c.note_id join users u on u.idU = n.user_id join photos p on n.idN = p.note_id where s.degreeCourse_id ='$idDc[0]' group by c.note_id order by c.note_id
+                                                )  t1  join (SELECT  note_id as idN,count(note_id) as page FROM  AqNoteApi.photos join  notes n on n.idN = photos.idP group by note_id)  t2
                                                  on t1.idN = t2.idN order by t1.nameS;")->getValue());
         //  $prova= collect($hello)->groupBy('nameS');//->groupBy('year')->groupBy('nameS');//;
 
